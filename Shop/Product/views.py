47cd0bb926a6e71a25dd5e2d4abcda73b742ad27
remble1 <pRt8zfuPUwb9>
2022-03-17 20:50:30 +0100

    home page, admin, database work well

--- Shop/Product/views.py
+++ Shop/Product/views.py
@@ -4,6 +4,6 @@ from .models import Product
 
 # Create your views here.
 
-def index(request): # pod odpytaniu wypluje zawartosc baz
+def index(request): # pod odpytaniu wypluje cala zawartosc baz
     request_return = Product.objects.all()
     return HttpResponse(request_return)
