b5a0024c433427a09428e04747bd7eeb7346c4c0
remble1 <pRt8zfuPUwb9>
2022-03-17 20:44:14 +0100

    add index funct to views to return all database - just for test

--- Shop/Product/views.py
+++ Shop/Product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+
+def index(request): # pod odpytaniu wypluje zawartosc baz
+    request_return = Product.objects.all()
+    return HttpResponse(request_return)
