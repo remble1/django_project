df6cb54bbc082570c92a4d152634675cf0b23ae9
remble1 <pRt8zfuPUwb9>
2022-03-24 21:49:06 +0100

    I added ForeginKey into product - producent

--- Shop/Product/models.py
+++ Shop/Product/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
-    def __str__(self):
-        return self.nazwa # obj Product return name
-
-    nazwa = models.CharField(max_length=100)
-    opis = models.TextField(blank=True)
-    cena = models.DecimalField(max_digits=12, decimal_places=2)
-
 class Producent(models.Model):
     def __str__(self):
         return self.nazwa
 
     nazwa = models.CharField(max_length=60)
     opis = models.TextField(blank=True)
+
+class Product(models.Model):
+    def __str__(self):
+        return self.nazwa # obj Product return name
+
+    producent = models.ForeignKey(Producent, on_delete=models.CASCADE, null=True)
+    nazwa = models.CharField(max_length=100)
+    opis = models.TextField(blank=True)
+    cena = models.DecimalField(max_digits=12, decimal_places=2)
+
 
