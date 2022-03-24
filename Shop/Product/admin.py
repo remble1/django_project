2dd25cd327b95ba52d61758bc94d3e3c6bd4ff92
remble1 <pRt8zfuPUwb9>
2022-03-24 21:40:16 +0100

    I make new table in database for products from other shops

--- Shop/Product/admin.py
+++ Shop/Product/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Product
+
 # Register your models here.
 
+from .models import Product, Producent
+
 admin.site.register(Product)
+admin.site.register(Producent)
--- Shop/Product/models.py
+++ Shop/Product/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    def __str__(self):
+        return self.nazwa # obj Product return name
+
     nazwa = models.CharField(max_length=100)
     opis = models.TextField(blank=True)
     cena = models.DecimalField(max_digits=12, decimal_places=2)
+
+class Producent(models.Model):
     def __str__(self):
         return self.nazwa
+
+    nazwa = models.CharField(max_length=60)
+    opis = models.TextField(blank=True)
+
