8f368fd7fe6016972ff6d4fb7dc616deaff7e613
remble1 <pRt8zfuPUwb9>
2022-04-04 21:45:53 +0200

    added first index template and iterable json with all category

--- Shop/Product/views.py
+++ Shop/Product/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 from .models import Product, Category
 
 # Create your views here.
 
 def index(request): # pod odpytaniu wypluje cala zawartosc baz
-    # wszystkie = Product.objects.all()
-    jeden = Product.objects.filter(kategoria=1)
-    kat_name = Category.objects.get(id=2)
-    kat_all = Category.objects.all()
-    return HttpResponse(jeden)
+
+    # jeden = Product.objects.filter(kategoria=1)
+    category = Category.objects.all()
+    dane = {'category' : category}
+    return render(request, 'szablon.html', dane)
 
 def category(request, id):
     category_user = Category.objects.get(pk=id)
@@ -20,4 +21,5 @@ def product(request, id):
     sample = '<h1>' + str(product_user) + '</h1>' + \
              '<p>' + str(product_user.opis) + '<p>' + \
              '<p>' + str(product_user.cena) + '<p>'
+    # to tylko test, tak nie można robić
     return HttpResponse(sample)
--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'D:\workspace\django_project\Shop\Product\template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
